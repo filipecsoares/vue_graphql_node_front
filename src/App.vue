ac4c628d74468e7e0d67a074dcf8f8bd13a06bcc
Filipe Soares <filipecarlossoares@gmail.com>
2019-10-08 20:41:40 -0300

    Using computed property

--- src/App.vue
+++ src/App.vue
@@ -66,8 +66,17 @@
 				<div class="card">
 					<div class="card-body">
 						<ul class="list-group">
-							<li class="list-group-item" v-for="domain in domains" v-bind:key="domain">
-								{{ domain }}
+							<li class="list-group-item" v-for="domain in domains" v-bind:key="domain.name">
+								<div class="row">
+									<div class="col-md">
+										{{ domain.name }}
+									</div>
+									<div class="col-md text-right">
+										<a class="btn btn-info" v-bind:href="domain.checkout" target="_blank">
+											<span class="fa fa-shopping-cart"></span>
+										</a>
+									</div>
+								</div>
 							</li>
 						</ul>
 					</div>
@@ -88,36 +97,42 @@ export default {
 			prefix: "",
 			sufix: "",
 			prefixes: ["Air", "Jet", "Flight"],
-			sufixes: ["Hub", "Station", "Mart"],
-			domains: ["AirHub", "AirStation", "AirMart", "JetHub", "JetStation", "JetMart", "FlightHub", "FlighStation", "FlightMart"]
+			sufixes: ["Hub", "Station", "Mart"]
 		};
 	},
 	methods: {
 		addPrefix(prefix) {
 			this.prefixes.push(prefix);
 			this.prefix = "";
-			this.generate();
 		},
 		deletePrefix(prefix) {
 			this.prefixes.splice(this.prefixes.indexOf(prefix), 1);
-			this.generate();
 		},
 		addSufix(sufix) {
 			this.sufixes.push(sufix);
 			this.sufix = "";
-			this.generate();
 		},
 		deleteSufix(sufix) {
 			this.sufixes.splice(this.sufixes.indexOf(sufix), 1);
-			this.generate();
-		},
-		generate() {
-			this.domains = [];
+		}
+	},
+	computed: {
+		domains() {
+			const domains = [];
 			for (const prefix of this.prefixes) {
 				for (const sufix of this.sufixes) {
-					this.domains.push(prefix + sufix);
+					const name = prefix + sufix;
+					const url = name.toLowerCase();
+					const checkout = `https://checkout.hostgator.com.br/?a=add&sld=${url}&tld=.com.br`;
+					const domain = {
+						name,
+						url,
+						checkout
+					};
+					domains.push(domain);
 				}
 			}
+			return domains;
 		}
 	}
 };
