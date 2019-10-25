beca83690c8d8f797df2579eb6c43c234fbf129e
Filipe Soares <filipecarlossoares@gmail.com>
2019-10-24 21:16:43 -0300

    Trabalhando com axios

--- src/components/DomainList.vue
+++ src/components/DomainList.vue
@@ -1,43 +1,38 @@
 <template>
-	<div>
-		<div id="main">
-			<div class="container">
-				<div class="row">
-					<div class="col-md">
-						<AppItemList title="Prefixos" v-bind:items="prefixes" v-on:addItem="addPrefix" v-on:deleteItem="deletePrefix"></AppItemList>
-					</div>
-					<div class="col-md">
-						<AppItemList title="Sufixos" v-bind:items="sufixes" v-on:addItem="addSufix" v-on:deleteItem="deleteSufix"></AppItemList>
-					</div>
-				</div>
-				<br/>
-				<h5>Domínios <span class="badge badge-info">{{ domains.length }}</span></h5>
-				<div class="card">
-					<div class="card-body">
-						<ul class="list-group">
-							<li class="list-group-item" v-for="domain in domains" v-bind:key="domain.name">
-								<div class="row">
-									<div class="col-md">
-										{{ domain.name }}
-									</div>
-									<div class="col-md text-right">
-										<a class="btn btn-info" v-bind:href="domain.checkout" target="_blank">
-											<span class="fa fa-shopping-cart"></span>
-										</a>
-									</div>
-								</div>
-							</li>
-						</ul>
-					</div>
-				</div>
+	<div class="container">
+		<div class="row">
+			<div class="col-md">
+				<AppItemList title="Prefixos" v-bind:items="prefixes" v-on:addItem="addPrefix" v-on:deleteItem="deletePrefix"></AppItemList>
+			</div>
+			<div class="col-md">
+				<AppItemList title="Sufixos" v-bind:items="sufixes" v-on:addItem="addSufix" v-on:deleteItem="deleteSufix"></AppItemList>
+			</div>
+		</div>
+		<br/>
+		<h5>Domínios <span class="badge badge-info">{{ domains.length }}</span></h5>
+		<div class="card">
+			<div class="card-body">
+				<ul class="list-group">
+					<li class="list-group-item" v-for="domain in domains" v-bind:key="domain.name">
+						<div class="row">
+							<div class="col-md">
+								{{ domain.name }}
+							</div>
+							<div class="col-md text-right">
+								<a class="btn btn-info" v-bind:href="domain.checkout" target="_blank">
+									<span class="fa fa-shopping-cart"></span>
+								</a>
+							</div>
+						</div>
+					</li>
+				</ul>
 			</div>
 		</div>
 	</div>
 </template>
 
 <script>
-import "bootstrap/dist/css/bootstrap.css";
-import "font-awesome/css/font-awesome.css";
+import axios from "axios/dist/axios";
 import AppItemList from "./AppItemList";
 export default {
 	name: "app",
@@ -46,8 +41,8 @@ export default {
 	},
 	data: function () {
 		return {
-			prefixes: ["Air", "Jet", "Flight"],
-			sufixes: ["Hub", "Station", "Mart"]
+			prefixes: [],
+			sufixes: []
 		};
 	},
 	methods: {
@@ -80,6 +75,30 @@ export default {
 			}
 			return domains;
 		}
+	},
+	created() {
+		axios({
+			url: "http://localhost:4000",
+			method: "post",
+			data: {
+				query: `
+					{
+						prefixes: items (type: "prefix") {
+							id
+							type
+							description
+						}
+						sufixes: items (type: "sufix") {
+							description
+						}
+					}
+				`
+			}
+		}).then(response => {
+			const query = response.data;
+			this.prefixes = query.data.prefixes.map(prefix => prefix.description);
+			this.sufixes = query.data.sufixes.map(sufix => sufix.description);
+		});
 	}
 };
 </script>
